68abfa6fbf0b85184246436be81242afab1eb701
Mays Akram AlDadah <maysaldadah@gmail.com>
2023-07-27 17:41:39 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,9 @@ Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update']
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')->name('secretpage')->middleware('verified');
 
+
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
-Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage')->name('verysecretpage')->middleware('password.confirm');
+Route::view('/verysecretpage', 'verysecretpage')->name('verysecretpage')->middleware('password.confirm');
+
 require __DIR__.'/auth.php';
